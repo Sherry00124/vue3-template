ae0665e2cd7bf20fc2184b9f493ace0019ad9fab
Sherry00124 <511210839@qq.com>
2023-12-06 15:22:52 +0800

    修复打包后路由问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import i18n from "@/i18n/index";
 import {
   OrderedListOutlined,
@@ -71,7 +71,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
